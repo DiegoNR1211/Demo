d6bf41b0b2785d2f780e16b1ff393f291ead6e0c
Joseph <joseph.avalos@zp.com.pe>
2021-06-17 18:03:01 -0500

    Consulta ruc proyecto

---
+++ Consulta RUC .NET/ZP.ConsultaRUC.Sync/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ZP.ConsultaRUC.Sync
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
