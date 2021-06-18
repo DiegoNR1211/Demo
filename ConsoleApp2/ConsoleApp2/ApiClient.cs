e81c2549a53fa4f1b6c82f0e12fbb85bc0cdd839
JairoSivincha <jairo.sivincha@zp.com.pe>
2021-06-18 09:25:18 -0500

    Demo wsp

---
+++ ConsoleApp2/ConsoleApp2/ApiClient.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace ConsoleApp2
+{
+    public class APIClient
+    {
+        public class ContentType
+        {
+            public const String JSON = "application/json";
+            public const String FORM = "application/x-www-form-urlencoded";
+        }
+
+        public class RequestTypeHttp
+        {
+            public const String GET = "GET";
+            public const String POST = "POST";
+            public const String DELETE = "DELETE";
+        }
+
+        public static JObject Request(String URL, String ContentType, String Authorization, JObject Parameters, String RequestType)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
+            request.Method = RequestType;
+            request.ContentType = ContentType;
+
+            if (Authorization != null)
+                request.Headers.Add(HttpRequestHeader.Authorization, Authorization);
+
+            if (Parameters != null)
+            {
+                StreamWriter objStreamWriter = new StreamWriter(request.GetRequestStream());
+                objStreamWriter.Write(Parameters.ToString());
+                objStreamWriter.Flush();
+                objStreamWriter.Close();
+            }
+
+            try
+            {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                StreamReader objStreamReader = new StreamReader(response.GetResponseStream());
+                string responseString = objStreamReader.ReadToEnd();
+                if (!string.IsNullOrWhiteSpace(responseString))
+                {
+                    JObject jResult = JObject.Parse(responseString);
+                    return jResult;
+                }
+                else
+                    return new JObject();
+            }
+            catch (WebException ex)
+            {
+                StreamReader objStreamReader = new StreamReader(ex.Response.GetResponseStream());
+                JObject jResult = JObject.Parse(objStreamReader.ReadToEnd());
+                return jResult;
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,75 @@
+﻿using ConsoleApp2;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace ChatApi
+{
+    public class Program
+    {
+        const string UrlAPI = "https://api.chat-api.com/instance288897/";
+        const string Token = "gatxr0kt1tb3mmhx";
+        const string ApiMensaje = "sendMessage";
+        const string ApiArchivo = "sendFile";
+
+        static void Main(string[] args)
+        {
+            EnviarMensaje();
+            //EnviarArchivo();      
+        }
+
+        public static void EnviarMensaje()
+        {
+            Console.WriteLine("Ingrese número del destinatario:");
+            string Numero = Console.ReadLine();
+
+            Console.WriteLine("Escriba el mensaje:");
+            string Mensaje = Console.ReadLine();
+
+            JObject jParameters = new JObject
+            {
+                ["phone"] = Numero,
+                ["body"] = Mensaje
+            };
+
+            string Url = UrlAPI + ApiMensaje + "?token=" + Token;
+            JObject jChatApiResult = APIClient.Request(Url, APIClient.ContentType.JSON, null, jParameters, APIClient.RequestTypeHttp.POST);
+
+            if ((bool)jChatApiResult["sent"])
+                Console.WriteLine("Mensaje envíado correctamente");
+            else
+                Console.WriteLine("Ocurrió un error al enviar mensaje: " + jChatApiResult["message"]);
+        }
+
+        public static void EnviarArchivo()
+        {
+            Console.WriteLine("Ingrese número del destinatario:");
+            string Numero = Console.ReadLine();
+
+            Console.WriteLine("Escriba el mensaje(opcional):");
+            string Mensaje = Console.ReadLine();
+
+            Console.WriteLine("Ingrese la url del archivo:");
+            string urlArchivo = Console.ReadLine();
+
+            //Console.WriteLine("Ingrese el nombre del archivo:");
+            //string nombreArchivo = Console.ReadLine();
+            //https://empresas.blogthinkbig.com/wp-content/uploads/2019/11/Imagen3-245003649.jpg?w=800
+
+            JObject jParameters = new JObject
+            {
+                ["phone"] = Numero,
+                ["body"] = urlArchivo,
+                ["filename"] = "4x4pixel.jpg",
+                ["caption"] = Mensaje
+            };
+
+            string Url = UrlAPI + ApiArchivo + "?token=" + Token;
+            JObject jChatApiResult = APIClient.Request(Url, APIClient.ContentType.JSON, null, jParameters, APIClient.RequestTypeHttp.POST);
+
+            if ((bool)jChatApiResult["sent"])
+                Console.WriteLine("Mensaje envíado correctamente");
+            else
+                Console.WriteLine("Ocurrió un error al enviar mensaje: " + jChatApiResult["message"]);
+        }
+    }
+}
